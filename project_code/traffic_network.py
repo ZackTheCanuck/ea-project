56f2e3572827f0317612356334ba82e06a59a973
Zack Urbaniak <zurbaniak@rogers.com>
2023-02-08 11:59:38 -0500

    Improve traffic_network code structure
    - Update graph creation to be object oriented
    - Fix issue where we couldn't access csvs

--- project_code/traffic_network.py
+++ project_code/traffic_network.py
@@ -4,74 +4,63 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import pandas as pd
 
-
-# insert the nodes into the graph
-def add_nodes(zones_dataframe):
-    
-    graph = nx.MultiDiGraph()
-    graph.add_nodes_from(zones_dataframe['MOVEMENT_ID'])
-
-    return graph
+import os
 
 
-# insert the edges into the graph, this is a separate function since we can have different weights for different times, days, months, etc
-def add_edges(graph, neighbours_dataframe, edges_dataframe, month):
+class graph():
+    def __init__(self, neighbours_csv, edge_weights_csv, filter_parameter=1) -> None:
+        self.df_neighbours = pd.read_csv(neighbours_csv)
+        self.df_edges      = pd.read_csv(edge_weights_csv)
+        self.graph         = nx.MultiDiGraph()
+        self.filter        = filter_parameter
 
-    id_list          = neighbours_dataframe['MOVEMENT_ID'].to_list()
-    neighbours_frame = neighbours_dataframe['NEIGHBORS'].to_list()
+    # insert the nodes into the graph
+    def add_graph_nodes(self, column='MOVEMENT_ID'):
+        self.graph.add_nodes_from(self.df_neighbours[column])
 
-    # keep track of our edges to adjacent zones
-    neighbours_dict = defaultdict(list)
-    for id_num, neighbours in zip(id_list, neighbours_frame):
-        ids_neighbours = list(map(int, neighbours.split(',')))
-        for neighbour in ids_neighbours:
-            neighbours_dict[id_num].append(neighbour)
+    # insert the edges into the graph
+    def add_graph_edges(self, weights='mean_travel_time'):
+        # extract the required fields to track neighbours
+        ids_list, neighbours_list = self.df_neighbours['MOVEMENT_ID'].to_list(), self.df_neighbours['NEIGHBORS'].to_list()
 
-    sources      = edges_dataframe['sourceid'].to_list()
-    destinations = edges_dataframe['dstid'].to_list()
-    travel_times = edges_dataframe['mean_travel_time'].to_list()
-    months       = edges_dataframe['month'].to_list()
+        # keep track of our edges to adjacent zones
+        self.neighbours_dict = defaultdict(list)
+        for id_num, neighbours in zip(ids_list, neighbours_list):
+            ids_neighbours = list(map(int, neighbours.split(',')))
+            for neighbour in ids_neighbours:
+                self.neighbours_dict[id_num].append(neighbour)
 
-    weight_values = []
-    for s, d, t, m in zip(sources, destinations, travel_times, months):
-        if (int(m) == month) and (d in neighbours_dict[s]):
-            edge_info = (s, d, t)
-            weight_values.append(edge_info)
-    graph.add_weighted_edges_from(weight_values, weight='mean_travel_time')
-    # print(graph.get_edge_data(107, 1))
-    # print(graph.edges.data())
+        # extract the required fields to insert edges into graph
+        sources, destinations, travel_times, filter_by = self.df_edges['sourceid'].to_list(), self.df_edges['dstid'].to_list(), self.df_edges[weights].to_list(), self.df_edges['month'].to_list()
+        weight_values = []
+        for s, d, t, f in zip(sources, destinations, travel_times, filter_by):
+            if (int(f) == self.filter) and (d in self.neighbours_dict[s]):
+                edge_info = (s, d, t)
+                weight_values.append(edge_info)
+        self.graph.add_weighted_edges_from(weight_values, weight=weights)
 
-    return graph, neighbours_dict
+    def build(self):
+        self.add_graph_nodes()
+        self.add_graph_edges()
+        print(self.graph)
 
-
-# build our NetworkX graph
-def build_graph(neighbours_csv, edge_weights_csv, month):
-    
-    df_neighbours = pd.read_csv(neighbours_csv)
-    df_edges = pd.read_csv(edge_weights_csv)
-
-    toronto_graph = add_nodes(df_neighbours)
-    toronto_graph, neigbours_dict = add_edges(toronto_graph, df_neighbours, df_edges, month)
-    
-    return toronto_graph, neigbours_dict
-
-
-# use matplotlib to display a graph
-def display_graph(graph):
-
-    nx.draw(graph, pos=nx.nx_agraph.graphviz_layout(graph), node_size=200, node_color='lightblue',
+    def display(self):
+        nx.draw(self.graph, pos=nx.nx_agraph.graphviz_layout(self.graph), node_size=200, node_color='lightblue',
             linewidths=0.25, font_size=8, with_labels=True, arrowstyle='-')
-    # labels = nx.get_edge_attributes(graph,'mean_travel_time')
-    # nx.draw_networkx_edge_labels(graph, pos, edge_labels=labels)
-    plt.show()
+        plt.show()
 
+    def get_neighbours(self):
+        return self.neighbours_dict
+    
 
 def main():
-    
-    toronto_graph, neigbours_dict = build_graph('toronto_neighbourhood_neighbours.csv', 'toronto-neighbourhoods-2020-1-OnlyWeekdays-MonthlyAggregate.csv', 1)
-    print(toronto_graph)
-    # print(neigbours_dict)
-    # display_graph(toronto_graph)
+    neighbourhood_neighbours   = 'project_code/csv_files/toronto_neighbourhood_neighbours.csv'
+    neighbourhood_travel_times = 'project_code/csv_files/toronto-neighbourhoods-2020-1-OnlyWeekdays-MonthlyAggregate.csv'
+    toronto_graph = graph(neighbourhood_neighbours, neighbourhood_travel_times, 1)
+    toronto_graph.build()
+    toronto_neighbours = toronto_graph.get_neighbours()
+    print(toronto_neighbours)
+    toronto_graph.display()
 
 
 if __name__ == "__main__":
