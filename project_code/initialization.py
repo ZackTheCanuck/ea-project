ee502a32a14e5aad49cd798d0dcb87ca48265315
FawazKh <fawaz.khalid98@gmail.com>
2023-03-18 18:22:06 -0400

    Creating initialization function

--- project_code/initialization.py
+++ project_code/initialization.py
@@ -1,5 +1,24 @@
-# initialize set of random routes from decided points
-# currently hardcoded in main, can move here if reason to do so
+import random
 
-def generate_routes(start, end, routes_per_member):
-    return
+#initialize population with random routes
+def initialize_population(graph, start, end, population_size, n_routes):
+    population = []
+    for i in range(population_size):
+        individual = []
+        for x in range(n_routes):
+            route = random_route(graph, start, end)
+            individual.append(route)
+        population.append(individual)
+    return population
+
+#Generate random routes between start and end nodes in the graph
+def random_route(graph, start, end):
+    route = [start]
+    current_node = start
+    while current_node != end:
+        neighbors = list(graph.neighbors(current_node))
+        next_node = random.choice(neighbors)
+        route.append(next_node)
+        current_node = next_node
+    return route
+
