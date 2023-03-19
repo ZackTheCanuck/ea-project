f28912ee49f80bca9699d4faed9a1d0714f8ef93
FawazKh <fawaz.khalid98@gmail.com>
2023-03-19 13:06:14 -0400

    Update mutation.py

--- project_code/mutation.py
+++ project_code/mutation.py
@@ -2,6 +2,7 @@
 import ea_helpers
 import numpy as np
 import shortest_path_algorithms
+import random
 
 
 # select a route to mutate inversely proportional to how often the route appears in the individual
@@ -21,8 +22,40 @@ def new_route(individual, all_routes, graph, start, end):
     
     return individual
 
-def random_p(individual, all_routes, graph, start, end):
-    return individual
+
+def random_p(individual, all_unique_routes, graph, start_node, end_node):
+    temp_ind = individual
+    try:
+        # Choose a random subset of routes proportional to their inverse traffic flow
+        probabilities = ea_helpers.get_inverse_traffic_flow_probabilities(individual, all_unique_routes)
+        selected_routes = np.random.choice(individual.routes, size=len(individual.routes) // 2, replace=False, p=probabilities)
+
+        # Replace subsegments of each selected route
+        for route in selected_routes:
+            # Choose a start node and destination node
+            start_index = random.randint(0, len(route) - 1)
+            route_length = len(route)
+            end_index = start_index + round(np.random.normal(0.25 * route_length, 0.5 * route_length))
+            end_index = min(max(end_index, 0), route_length - 1)  # Ensure destination index is within valid range
+
+            # Replace subsegment with a new random route
+            new_route = shortest_path_algorithms.dijkstra(graph, route[start_index], route[end_index])
+            individual.routes[individual.routes.index(route)] = new_route
+
+        # Update all_unique_routes with new routes
+        for route in individual.routes:
+            if route not in all_unique_routes:
+                all_unique_routes.append(route)
+    except Exception:
+        pass
+
+    return temp_ind
+
+
+
+
+
+
 
 def link_wp(individual, all_routes, graph, start, end):
     return individual
