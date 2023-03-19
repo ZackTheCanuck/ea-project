c0f22cfd3a391fb51a00ca34dad6a880695af1d1
FawazKh <fawaz.khalid98@gmail.com>
2023-03-19 13:33:46 -0400

    Update mutation.py

--- project_code/mutation.py
+++ project_code/mutation.py
@@ -12,9 +12,7 @@ def new_route(individual, all_routes, graph, start, end):
     probabilities    = ea_helpers.get_inverse_traffic_flow_probabilities(individual, all_routes)
     all_routes_index = np.random.choice(range(len(all_routes)), p=probabilities)
     route_to_replace = all_routes[all_routes_index]
-    
     route_index = individual.routes.index(route_to_replace)
-    
     new_route = shortest_path_algorithms.dijkstra(graph, start, end)
     individual.routes[route_index] = new_route
     if new_route not in all_routes:
@@ -24,32 +22,31 @@ def new_route(individual, all_routes, graph, start, end):
 
 
 def random_p(individual, all_unique_routes, graph, start_node, end_node):
-    temp_ind = individual
-    try:
-        # Choose a random subset of routes proportional to their inverse traffic flow
-        probabilities = ea_helpers.get_inverse_traffic_flow_probabilities(individual, all_unique_routes)
-        selected_routes = np.random.choice(individual.routes, size=len(individual.routes) // 2, replace=False, p=probabilities)
 
-        # Replace subsegments of each selected route
-        for route in selected_routes:
-            # Choose a start node and destination node
-            start_index = random.randint(0, len(route) - 1)
-            route_length = len(route)
-            end_index = start_index + round(np.random.normal(0.25 * route_length, 0.5 * route_length))
-            end_index = min(max(end_index, 0), route_length - 1)  # Ensure destination index is within valid range
+    # Choose a random subset of routes proportional to their inverse traffic flow
+    probabilities = ea_helpers.get_inverse_traffic_flow_probabilities(individual, all_unique_routes)
+    # Select a random subset of route indices from all_unique_routes based on probabilities
+    selected_routes_indices = np.random.choice(len(all_unique_routes), size=len(individual.routes) // 2, replace=False, p=probabilities)
+    # Get the routes corresponding to the selected indices
+    selected_routes = []
+    for i in selected_routes_indices:
+        selected_routes.append(all_unique_routes[i])
+    for route in selected_routes:
+        # Choose a start node and destination node
+        start_index = random.randint(0, len(route) - 1)
+        route_length = len(route)
+        end_index = start_index + round(np.random.normal(0.25 * route_length, 0.5 * route_length))
+        end_index = min(max(end_index, 0), route_length - 1)  # make sure end is in range
+        # Replace subsegment with a new random route
+        new_route = shortest_path_algorithms.dijkstra(graph, route[start_index], route[end_index])
+        individual.routes[individual.routes.index(route)] = new_route
 
-            # Replace subsegment with a new random route
-            new_route = shortest_path_algorithms.dijkstra(graph, route[start_index], route[end_index])
-            individual.routes[individual.routes.index(route)] = new_route
+    # Update all_unique_routes with new routes
+    for route in individual.routes:
+        if route not in all_unique_routes:
+            all_unique_routes.append(route)
 
-        # Update all_unique_routes with new routes
-        for route in individual.routes:
-            if route not in all_unique_routes:
-                all_unique_routes.append(route)
-    except Exception:
-        pass
-
-    return temp_ind
+    return individual
 
 
 
